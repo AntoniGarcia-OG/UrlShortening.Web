5579a62ba5af4d169463f7e27e6ff02a7efb6193
Antoni Ortiz <antonigarcia.og@gmail.com>
2026-01-29 19:37:05 -0600

    Add CreateAsync method to CodeService and Create page for URL mapping

--- UrlShortening.Web/Services/CodeService.cs
+++ UrlShortening.Web/Services/CodeService.cs
@@ -1,4 +1,8 @@
-﻿namespace UrlShortening.Web.Services
+﻿using System.Net.Http.Json;
+using UrlShortening.Web.DTOs.Requests;
+using UrlShortening.Web.DTOs.Responses;
+
+namespace UrlShortening.Web.Services
 {
     public class CodeService
     {
@@ -7,6 +11,23 @@
         public CodeService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<CodeResponseDto?> CreateAsync(string originalUrl)
+        {
+            CreateCodeDto dto = new()
+            {
+                OriginalUrl = originalUrl
+            };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/api/UrlMapping", dto);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<CodeResponseDto>();
         }
     }
 }
